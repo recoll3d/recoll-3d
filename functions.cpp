6ab96daaf847258597b3247cd6f8b232cd515aaa
Nelson Victor <snelsonvictor68@gmail.com>
2023-11-13 12:48:46 -0300

    Refatoração e Adições: Máquina de Coleta e Backend.
    ◆ ESP32 (Máquina de Coleta):
    
    ▪ Separei funções do arquivo principal, criando um arquivo separado.
    ▪ Centralizei variáveis globais em um único arquivo.
    ▪ Alterei a lógica das funções de reciclagem para se adequarem ao uso de sensores de movimento e distância, substituindo chaves de fim de curso.
    ▪ Temporariamente, adicionei uma lógica de envio de dados (tipo de garrafa, pontos, endereço MAC) para o backend por meio de um botão, enquanto aguardo a implementação dos sensores.
    
    ◆ Backend:
    
    ▪ Modifiquei o esquema do banco de dados:
    ▪ Adicionei a tabela Recycling para armazenar informações sobre reciclagens, como quantidade de garrafas, pontuação total, etc.
    ▪ Introduzi a tabela Bottles para armazenar detalhes específicos sobre as garrafas recicladas.
    ▪ Implementei a funcionalidade de iniciar a reciclagem, com tempo de início e fim.
    ▪ Ao iniciar a reciclagem, os usuários podem inserir garrafas, que serão armazenadas na tabela Bottles.
    ▪ Após o término da reciclagem, uma rota atualiza a data de término, armazenando a quantidade e pontuação total de garrafas.
    ▪ Ainda não implementei a funcionalidade de tempo limite para a inserção de garrafas.
    ▪ Experimentei armazenar o token do usuário nos cookies para autenticação, mas enfrentei desafios na recuperação do token dentro do socket.io.
    ▪ Adicionei a funcionalidade de criar níveis e recompensas de nível.
    ▪ Corrigi a funcionalidade mode: "insensitive" do Prisma que estava impedindo o cadastro de mais usuários.

---
+++ functions.cpp
@@ -0,0 +1,277 @@
+#include "functions.h"
+#include "global_variables.h"
+
+#include <ArduinoJson.h>
+
+SocketIOclient socketIO;
+
+bool eventoEnviado = false;
+
+void socketIOEvent(socketIOmessageType_t type, uint8_t *payload, size_t length)
+{
+  switch (type)
+  {
+  case sIOtype_DISCONNECT:
+    USE_SERIAL.printf("[IOc] Disconnected!\n");
+    break;
+  case sIOtype_CONNECT:
+  {
+    USE_SERIAL.printf("[IOc] Connected to url: %s\n", payload);
+
+    // join default namespace (no auto join in Socket.IO V3)
+    socketIO.send(sIOtype_CONNECT, "/");
+    break;
+  }
+  case sIOtype_EVENT:
+  {
+    char *sptr = NULL;
+    int id = strtol((char *)payload, &sptr, 10);
+
+    USE_SERIAL.printf("[IOc] get event: %s id: %d\n", payload, id);
+
+    if (id)
+    {
+      payload = (uint8_t *)sptr;
+    }
+
+    DynamicJsonDocument doc(1024);
+    DeserializationError error = deserializeJson(doc, payload, length);
+
+    if (error)
+    {
+      USE_SERIAL.print(F("deserializeJson() failed: "));
+      USE_SERIAL.println(error.c_str());
+      return;
+    }
+
+    String eventName = doc[0];
+    USE_SERIAL.printf("[IOc] event name: %s\n", eventName.c_str());
+
+    // Message Includes a ID for a ACK (callback)
+    if (id)
+    {
+      // creat JSON message for Socket.IO (ack)
+      DynamicJsonDocument docOut(1024);
+      JsonArray array = docOut.to<JsonArray>();
+
+      // add payload (parameters) for the ack (callback function)
+      JsonObject param1 = array.createNestedObject();
+      param1["now"] = millis();
+
+      // JSON to String (serializion)
+      String output;
+      output += id;
+      serializeJson(docOut, output);
+
+      // Send event
+      socketIO.send(sIOtype_ACK, output);
+    }
+
+    if (String(eventName) == "turn_on_led")
+    {
+      // Processar o valores recebido
+
+      // Acessar o segundo elemento do array JSON
+      JsonObject obj = doc[1];
+
+      // Acessar a chave "on" no objeto JSON
+      bool active = obj["on"];
+
+      USE_SERIAL.printf("Ativo: %s\n", active ? "Verdadeiro" : "Falso");
+      digitalWrite(BLUE_LED, active);
+    }
+  }
+  break;
+  case sIOtype_ACK:
+    USE_SERIAL.printf("[IOc] get ack: %u\n", length);
+    break;
+  case sIOtype_ERROR:
+    USE_SERIAL.printf("[IOc] get error: %u\n", length);
+    break;
+  case sIOtype_BINARY_EVENT:
+    USE_SERIAL.printf("[IOc] get binary: %u\n", length);
+    break;
+  case sIOtype_BINARY_ACK:
+    USE_SERIAL.printf("[IOc] get binary ack: %u\n", length);
+    break;
+  }
+}
+
+unsigned long messageTimestamp = 0;
+
+void sendEventName()
+{
+  uint64_t now = millis();
+
+  if (now - messageTimestamp > 2000)
+  {
+    messageTimestamp = now;
+
+    // creat JSON message for Socket.IO (event)
+    DynamicJsonDocument doc(1024);
+    JsonArray array = doc.to<JsonArray>();
+
+    // add event name
+    // Hint: socket.on('event_name', ....
+    array.add("event_name");
+
+    // add payload (parameters) for the event
+    JsonObject param1 = array.createNestedObject();
+    param1["now"] = (uint32_t)now;
+
+    // JSON to String (serializion)
+    String output;
+    serializeJson(doc, output);
+
+    // Send event
+    socketIO.sendEVENT(output);
+
+    // Print JSON for debugging
+    USE_SERIAL.println(output);
+  }
+}
+
+void sendMacAddress()
+{
+  // creat JSON message for Socket.IO (ack)
+  DynamicJsonDocument docOut(1024);
+  JsonArray array = docOut.to<JsonArray>();
+
+  array.add("register_point_collection");
+
+  // add payload (parameters) for the ack (callback function)
+  JsonObject param1 = array.createNestedObject();
+  param1["mac_address"] = String(WiFi.macAddress());
+
+  // JSON to String (serializion)
+  String output;
+  serializeJson(docOut, output);
+
+  USE_SERIAL.println(output);
+
+  // Envia o MAC para o servidor via Socket.IO
+  socketIO.sendEVENT(output);
+
+  delay(1000);
+}
+
+void handleRecycling()
+{
+  DynamicJsonDocument docOut(1024);
+  JsonArray array = docOut.to<JsonArray>();
+
+  array.add("register_recycling");
+
+  JsonObject param1 = array.createNestedObject();
+  param1["mac_address"] = String(WiFi.macAddress());
+  param1["number_of_bottles"] = 0;
+  param1["total_bottles_score"] = 0;
+
+  String output;
+  serializeJson(docOut, output);
+
+  USE_SERIAL.println(output);
+
+  socketIO.sendEVENT(output);
+
+  lowLevelBottle();
+
+  bool bottleDetected = digitalRead(LOW_START_KEY) == 1 && digitalRead(LOW_END_KEY) == 1 && !eventoEnviado;
+
+  // if (bottleDetected) {
+  //   DynamicJsonDocument docOut(1024);
+  //   JsonArray array = docOut.to<JsonArray>();
+
+  //   array.add("register_bottle");
+
+  //   JsonObject param1 = array.createNestedObject();
+  //   param1["level"] = 1;
+  //   param1["points"] = 15;
+
+  //   String output;
+  //   serializeJson(docOut, output);
+
+  //   USE_SERIAL.println("Você ganhou 15 pontos");
+  //   USE_SERIAL.println(output);
+
+  //   socketIO.sendEVENT(output);
+
+  //   eventoEnviado = true;
+  // } else if (digitalRead(LOW_START_KEY) == 0 || digitalRead(LOW_END_KEY) == 0) {
+  //   eventoEnviado = false;
+  // }
+
+  // =================================================================================
+
+  // bool valorPIR = digitalRead(pinPIR);
+
+  // if (valorPIR && !eventoEnviado) {
+  //   Serial.println("DETECTADO");
+  //   eventoEnviado = true;
+  // } else if (!valorPIR) {
+  //   Serial.println("---------");
+  //   eventoEnviado = false;
+  // }
+}
+
+void lowLevelBottle()
+{
+  // Conta os pontos quando as duas chaves são pressionadas simultaneamente.
+  bool bottleDetected2 = digitalRead(BUTTON_PIN);
+
+  if (bottleDetected2 && !eventoEnviado)
+  {
+    DynamicJsonDocument docOut(1024);
+    JsonArray array = docOut.to<JsonArray>();
+
+    array.add("register_bottle");
+
+    JsonObject param1 = array.createNestedObject();
+    param1["level"] = 1;
+    param1["points"] = 15;
+    param1["mac_address"] = String(WiFi.macAddress());
+
+    String output;
+    serializeJson(docOut, output);
+
+    USE_SERIAL.println("Você ganhou 15 pontos");
+    USE_SERIAL.println(output);
+
+    socketIO.sendEVENT(output);
+
+    eventoEnviado = true;
+  }
+  else if (!bottleDetected2)
+  {
+    eventoEnviado = false;
+  }
+}
+
+void middleLevelBottle(char *points)
+{
+  // Conta os pontos quando as duas chaves são pressionadas simultaneamente.
+  if (digitalRead(MID_START_KEY) == 1 && digitalRead(MID_END_KEY) == 1)
+  {
+    // return client.send(points);
+
+    Serial.print("Obrigado por reciclar");
+    Serial.println("Você ganhou 15 pontos");
+  }
+  // // Não conta os pontos se as duas chaves não acionarem ao mesmo tempo.
+  // } else if (digitalRead(chave3) == 0 && digitalRead(chave4) == 1 ){
+  //   Serial.print("Não acontece nada");
+
+  // } else if (digitalRead(chave3) == 1 && digitalRead(chave4) == 0 ){
+  //   Serial.print("Não acontece nada");
+
+  // } else if (digitalRead(chave3) == 1 && digitalRead(chave4) == 1 ){
+  //   Serial.print("Não acontece nada");
+  // }
+}
+
+void highLevelBottle(char *points)
+{
+  // Conta os pontos quando as duas chaves são pressionadas simultaneamente.
+  Serial.print("Obrigado por reciclar");
+  Serial.println("Você ganhou 20 pontos");
+}
---
+++ global_variables.h
@@ -0,0 +1,20 @@
+#ifndef GLOBAL_VARIABLES
+#define GLOBAL_VARIABLES
+
+#define USE_SERIAL Serial
+
+#define BLUE_LED 2
+#define GIFT_RELEASE_RELAY 5
+
+#define HIGH_START_KEY 13
+#define HIGH_END_KEY 12
+
+#define MID_START_KEY 14
+#define MID_END_KEY 27
+
+#define LOW_START_KEY 25
+#define LOW_END_KEY 33
+
+#define BUTTON_PIN 32
+
+#endif
